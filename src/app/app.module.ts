d110978401a1bf5d89e008cdf78e401682a0ade5
pkevin24 <70275725+pkevin24@users.noreply.github.com>
2022-05-22 14:29:56 +0530

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AngularTypewriterEffectModule } from 'angular-typewriter-effect';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -18,7 +18,8 @@ import { AllsongsComponent } from './allsongs/allsongs.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularTypewriterEffectModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  list = ['Welcome to the MUSIC WORLD', 'which consists of MILLIONS of songs and PODCASTS from different creator']
   constructor() { }
 
   ngOnInit(): void {
